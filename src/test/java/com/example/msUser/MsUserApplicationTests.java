29e7a96695a0b036999277663641ba8ce455f96c
Williams <willyams095@gmail.com>
2022-10-01 23:55:37 -0500

    Initial commit

---
+++ src/test/java/com/example/msUser/MsUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.msUser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
